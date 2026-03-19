5e64b1c9ad6c601c29e36762eafa85d0a0cba269
domesya <163451587+domesya@users.noreply.github.com>
2026-03-19 19:09:44 +0100

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,13 +3,15 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q tauon-music-box | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q tauon-music-box-git | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/tauonmb.svg
 export DESKTOP=/usr/share/applications/tauonmb.desktop
+
+export DEPLOY_SYS_PYTHON=1
 
 # Deploy dependencies
 quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
