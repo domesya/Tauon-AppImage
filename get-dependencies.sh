d8a4bfb938bb429cd0c02e6037503764447b71f2
domesya <163451587+domesya@users.noreply.github.com>
2026-03-19 19:44:41 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,14 +6,14 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm pipewire-audio pipewire-jack python-jxlpy python-pypresence
+pacman -Syu --noconfirm tauon-music-box pipewire-audio pipewire-jack python-jxlpy python-pypresence
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package tauon-music-box-git
+#make-aur-package tauon-music-box-git
 
 # If the application needs to be manually built that has to be done down here
 
